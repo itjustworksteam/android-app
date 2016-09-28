70f83bde17a607ed9edda51f0a94f40898699077
Riccardo Crippa <therickys93@gmail.com>
2016-09-28 16:42:27 +0200

    primo commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# ANDROID APP #
+
+In this repository you find an android app that you can use to display a web page from a server. For example the web page of your company and so on...
+
+## What you have to modify ##
+
+You have to modify only the file named ```MainActivity.java``` the part of the constants:
+
+```
+private static String title = "Your title goes here";
+private static String URL = "Your URL goes here";
+private static String loading = "Your loading text goes here";
+```
---
+++ app/src/main/java/com/example/ricky/statoservizi/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.ricky.statoservizi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    private static String title = "Your title goes here";
+    private static String URL = "Your URL goes here";
+    private static String loading = "Your loading text goes here";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress < 100)
+                {
+                    setTitle(loading);
+                }
+                if(newProgress == 100)
+                {
+                    setTitle(title);
+                }
+            }
+        });
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(URL);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.action_button, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if(id == R.id.action_reload)
+        {
+            webView.reload();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
